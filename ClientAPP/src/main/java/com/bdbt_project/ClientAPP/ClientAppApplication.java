a85a60f386bd12f8c792e081197d072ab21ee285
Tomek <tomasz.wrobel2@gmail.com>
2023-01-07 21:16:55 +0100

    Łączy się z baza ale nie wiem co dalej zrobić

--- ClientAPP/src/main/java/com/bdbt_project/ClientAPP/ClientAppApplication.java
+++ ClientAPP/src/main/java/com/bdbt_project/ClientAPP/ClientAppApplication.java
@@ -1,6 +1,7 @@
 package com.bdbt_project.ClientAPP;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -9,8 +10,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 @SpringBootApplication
-public class ClientAppApplication {
-
+public class ClientAppApplication implements CommandLineRunner {
+@Autowired
+private JdbcTemplate jdbcTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(ClientAppApplication.class, args);
 
@@ -18,5 +20,14 @@ public class ClientAppApplication {
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+		String sql_stops = "select * from PRZYSTANKI";
+		List<Stop> stops = jdbcTemplate.query(sql_stops,BeanPropertyRowMapper.newInstance(Stop.class));
+		stops.forEach(System.out::println);
+	String sql_vehicles = "select * from MODELE natural join MARKI ";
+	List<Vehicle> vehicles = jdbcTemplate.query(sql_vehicles,BeanPropertyRowMapper.newInstance(Vehicle.class));
+	vehicles.forEach(System.out::println);
+	}
 
 }
---
+++ ClientAPP/src/main/java/com/bdbt_project/ClientAPP/Vehicle.java
@@ -0,0 +1,33 @@
+package com.bdbt_project.ClientAPP;
+
+import lombok.Data;
+
+
+public class Vehicle {
+    private int  nr_modelu;
+   private  String nazwa_modelu;
+
+    public int getNr_modelu() {
+        return nr_modelu;
+    }
+
+    public void setNr_modelu(int nr_modelu) {
+        this.nr_modelu = nr_modelu;
+    }
+
+    public String getNazwa_modelu() {
+        return nazwa_modelu;
+    }
+
+    public void setNazwa_modelu(String nazwa_modelu) {
+        this.nazwa_modelu = nazwa_modelu;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "nr_modelu=" + nr_modelu +
+                ", nazwa_modelu='" + nazwa_modelu + '\'' +
+                '}';
+    }
+}
